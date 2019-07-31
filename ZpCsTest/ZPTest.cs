be87d99ee9174bd17fd1c7056e8c1e2d3cb6e4cc
Zymlex <Zymlex@users.noreply.github.com>
2019-07-31 22:38:17 +1000

    initial commit

---
+++ ZpCsTest/ZPTest.cs
@@ -0,0 +1,24 @@
+﻿﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using ZennoLab.CommandCenter;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+
+namespace ZPTest
+{
+    public class Main : IZennoCustomCode, IZennoCustomEndCode
+    {
+        public int ExecuteCode(Instance instance, IZennoPosterProjectModel project)
+        {
+            project.SendInfoToLog("Test");
+
+            return 0;
+        }
+
+        public void GoodEnd(Instance instance, IZennoPosterProjectModel project) { }
+
+        public void BadEnd(Instance instance, IZennoPosterProjectModel project) { }
+    }
+}
---
+++ ZpFsTest/ZpTest.fs
@@ -0,0 +1,15 @@
+﻿namespace ZpTest
+
+open System
+open ZennoLab.CommandCenter
+open ZennoLab.InterfacesLibrary.ProjectModel
+
+type public Main() =
+    interface IZennoCustomCode with
+        member this.ExecuteCode(instance: Instance, project: IZennoPosterProjectModel): int =
+            project.SendInfoToLog "Test"
+            0
+
+    interface IZennoCustomEndCode with
+        member this.GoodEnd(instance: Instance, project: IZennoPosterProjectModel): unit = ()
+        member this.BadEnd(instance: Instance, project: IZennoPosterProjectModel): unit = ()
